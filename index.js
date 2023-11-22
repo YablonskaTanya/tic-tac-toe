53d23bd1188eacf9b792d03ba26d289e2405cff5
Yablonskaya_Tanya <t.n.yablonskaya@gmail.com>
2023-11-22 19:42:39 +0200

    add initial marckup

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".js-content");
+
+function createMarkup() {
+  let markup = "";
+  for (let i = 1; i < 10; i += 1) {
+    markup += `<div class="item js-item" data-id="${i}"></div>`;
+  }
+  container.innerHTML = markup;
+}
+createMarkup();
